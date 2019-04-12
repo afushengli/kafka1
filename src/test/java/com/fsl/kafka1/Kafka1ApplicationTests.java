8584c19d04e3baaa0398574e0f3ebbeb08117d17
chenrujun <chenrj@360bid.cn>
2019-04-12 17:50:32 +0800

    fist comit

---
+++ src/test/java/com/fsl/kafka1/Kafka1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fsl.kafka1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Kafka1ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
